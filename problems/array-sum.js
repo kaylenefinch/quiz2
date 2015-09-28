b27427ad2b815cbb2a30a79372e519079ee4b806
kaylenefinch <kaylenefinch@gmail.com>
2015-09-28 15:02:50 -0600

    Answers to quiz2 - KF

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function arraySum(arr) {
+  var total = 0;
+  for(var i = 0; i < arr.length; i++){
+    console.log(i + "  & Total = " + total);
 
+    if(typeof arr[i] == "object"){
+      arr[i] = arraySum(arr[i]);
+    }
+    total += arr[i];
+  }
+  return total;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,7 +1,14 @@
 'use strict';
 
 function longestString(arr) {
-
+  var longest = "";
+	for (var i=0; i < arr.length; i++) {
+    if (typeof arr[i] == "string") {
+          if (arr[i].length > longest.length){
+            longest = arr[i];
+          }
+        }
+      }
+      return longest;
 }
-
 module.exports = longestString;
